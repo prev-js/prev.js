51ec24314be9eb7cd60fb3a755d57fbb4ab6ef21
zoubingwu <zoubingwu@gmail.com>
2023-05-04 02:19:35 +0800

    Update playground code

---
+++ examples/playground/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { ComponentType } from "react";
+import { Link } from "prev.js";
+
+import "./global.css";
+
+export default function App({ Component }: { Component: ComponentType<{}> }) {
+  return (
+    <div className="app">
+      <div className="nav">
+        {["/", "/blog", "/404"].map((i) => (
+          <Link href={i} key={i}>
+            go to {i}
+          </Link>
+        ))}
+      </div>
+      <div className="content">
+        <Component />
+      </div>
+    </div>
+  );
+}
--- examples/playground/pages/index.tsx
+++ examples/playground/pages/index.tsx
@@ -1,10 +1,7 @@
-import { Link } from "prev.js";
-
 export default function Home() {
   return (
     <div>
       <h1>Hello world!</h1>
-      <Link href="/blog">go to blog</Link>
     </div>
   );
 }
