88c8004df5b5e8355ffbfc32a36d17a6a3d33707
zoubingwu <zoubingwu@gmail.com>
2023-05-15 22:38:05 +0800

    fix: typings

--- examples/playground/pages/index.tsx
+++ examples/playground/pages/index.tsx
@@ -1,4 +1,6 @@
 export default function Home() {
+  console.log(import.meta.env);
+
   return (
     <div>
       <h1>Hello world!</h1>
--- packages/prev/vite.d.ts
+++ packages/prev/vite.d.ts
@@ -1,1 +1,3 @@
+/// <reference types="vite/client" />
+
 export * from "./dist/export-vite";
